f8eda45cc6d6a6099b53f7f2f5204bcf6fb2716a
PioterAndrzejewski <109315248+PioterAndrzejewski@users.noreply.github.com>
2023-06-25 23:36:03 +0200

    add basic input and output

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,27 @@
 import ReactDOM from "react-dom";
+import { useState, useEffect } from "react";
 
 const App = () => {
-  return <h1>Hi!</h1>;
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+  useEffect(() => {
+    if (input !== "") {
+      setCode(input.toUpperCase());
+    }
+  }, [input]);
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
