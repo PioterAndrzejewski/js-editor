f426f2edb75ca6212e85d8147a1e1d6c20d3270c
PioterAndrzejewski <109315248+PioterAndrzejewski@users.noreply.github.com>
2023-06-28 13:07:42 +0200

    toggle editor style

--- src/components/MD-editor.tsx
+++ src/components/MD-editor.tsx
@@ -1,9 +1,35 @@
+import { useState, useEffect, useRef } from "react";
 import MDEditor from "@uiw/react-md-editor";
 
 const Editor: React.FC = () => {
+  const [editMode, setEditMode] = useState(false);
+  const editorRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const handleClick = (e: MouseEvent) => {
+      if (
+        editorRef.current &&
+        e.target &&
+        editorRef.current.contains(e.target as Node)
+      ) {
+        return;
+      }
+      setEditMode(false);
+    };
+    window.addEventListener("click", handleClick, { capture: true });
+    return () =>
+      window.removeEventListener("click", handleClick, { capture: true });
+  }, []);
+
   return (
-    <div>
-      <MDEditor.Markdown source='#fdsafEdea' />
+    <div
+      onClick={() => {
+        setEditMode(true);
+      }}
+      ref={editorRef}
+    >
+      {editMode && <MDEditor value='#fdsafEdea' />}
+      {!editMode && <MDEditor.Markdown source='#fdsafEdea' />}
     </div>
   );
 };
